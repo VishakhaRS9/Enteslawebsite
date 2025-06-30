2a0b0f1f6ab0385d5622b67826c2a81b652044d0
VishakhaRS9 <83124556+VishakhaRS9@users.noreply.github.com>
2025-06-30 17:21:16 +0530

    Add Netlify configuration

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
-}
+  reactStrictMode: true,
+};
 
-module.exports = nextConfig
+const { withNetlify } = require('@netlify/next');
+
+module.exports = withNetlify(nextConfig);
