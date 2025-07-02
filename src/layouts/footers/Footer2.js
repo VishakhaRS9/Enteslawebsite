24497e44c5a8ede6d81a6bfd914bcb9ca20d9704
VishakhaRS9 <83124556+VishakhaRS9@users.noreply.github.com>
2025-07-02 16:44:37 +0530

    Update Footer2.js

--- src/layouts/footers/Footer2.js
+++ src/layouts/footers/Footer2.js
@@ -1,120 +1,91 @@
 const Footer2 = () => {
   return (
-    <footer className="mil-dark-bg">
+     <footer className="mil-dark-bg">
       <img src="img/deco/map.png" alt="background" className="mil-footer-bg" />
       <div className="container">
         <div className="mil-footer-content">
-          <div className="row align-items-center mil-p-120-60">
-            <div className="col-xl-6 mil-mb-60">
-              <h3 className="mil-light mil-mb-15">
-                Join The <span className="mil-accent">ITSulu</span> Experience
-              </h3>
-              <p className="mil-light-soft">
-                Duis autem vel eum iriure dolor in hendrerit in vulputate.
-              </p>
-            </div>
-            <div className="col-xl-6 mil-mb-60">
-              <form className="mil-subscribe-form">
-                <input type="text" placeholder="Your email address" />
-                <button type="submit" className="mil-button mil-accent-bg">
-                  Subscribe Now
-                </button>
-              </form>
-            </div>
-          </div>
+        
           <div className="mil-divider mil-light" />
-          <div className="row justify-content-between mil-p-120-60">
-            <div className="col-md-3 col-lg-3 col-xl-3 mil-mb-30">
+          <div className="row  mil-p-120-60">
+            <div className="col-md-5 col-lg-5 col-xl-5 mil-mb-30">
               <img
-                src="img/logo/logo-light.png"
+                src="img/logo/entesla-logo_light.png"
                 alt="logo"
-                className="mil-logo mil-mb-60"
+                className="mil-logo mil-mb-30"
                 style={{ width: 140 }}
               />
               <p
                 className="mil-light mil-light-soft"
                 style={{ marginBottom: 45 }}
               >
-                2307 Beverley Rd Brooklyn, <br />
-                New York 11226 USA.
+                6/114, Mittal Estate, Marol,<br />
+Andheri East, Mumbai - 400059
+               
               </p>
               <ul className="mil-simple-list mil-mb-15">
                 <li className="mil-light">
-                  <span className="mil-accent">+07</span>&nbsp;
-                  <span className="mil-light-soft">0 002 00 05</span>
+                  <span className="mil-accent">(022) </span>&nbsp;
+                  <span className="mil-light-soft">4972 0910</span>
                 </li>
                 <li>
-                  <span className="mil-accent">info</span>
+                  <span className="mil-accent">contact</span>
                   <span className="mil-light mil-light-soft">
-                    @mydomain.com
+                    @entesla.com
                   </span>
                 </li>
               </ul>
             </div>
-            <div className="col-md-8 col-lg-7 col-xl-7">
-              <div className="row justify-content-end">
-                <div className="col-md-4 col-lg-4 mil-mb-60">
-                  <h4 className="mil-list-title mil-light mil-mb-60">Social</h4>
+            <div className="col-md-7 col-lg-7 col-xl-7">
+              <div className="row ">
+                <div className="col-md-7 col-lg-7 mil-mb-60">
+                  <h4 className="mil-list-title mil-light mil-mb-30">Services</h4>
                   <ul className="mil-hover-link-list mil-light">
                     <li>
-                      <a href="#.">Facebook</a>
+                      <a href="customDesign">Customised Design Services</a>
                     </li>
                     <li>
-                      <a href="#.">Instagram</a>
+                      <a href="iot">IoT</a>
                     </li>
                     <li>
-                      <a href="#.">LinkedIn</a>
+                      <a href="monitoring">Monitoring</a>
                     </li>
                     <li>
-                      <a href="#.">Twitter</a>
+                      <a href="mobileApp">Mobie App Development</a>
                     </li>
-                    <li>
-                      <a href="#.">YouTube</a>
-                    </li>
+                  
                   </ul>
                 </div>
-                <div className="col-md-4 col-lg-4 mil-mb-60">
-                  <h4 className="mil-list-title mil-light mil-mb-60">
-                    Support
+
+                <div className="col-md-5 col-lg-5 mil-mb-60">
+                  <h4 className="mil-list-title mil-light mil-mb-30">
+                    Case Studies
                   </h4>
                   <ul className="mil-hover-link-list mil-light">
                     <li>
-                      <a href="#.">Documentation</a>
+                      <a href="pwas">Proximity Warning Alarm System</a>
                     </li>
                     <li>
-                      <a href="#.">Support</a>
+                      <a href="dws">Dimensioning Weighing System</a>
                     </li>
                     <li>
-                      <a href="#.">FAQs</a>
+                      <a href="gds">Gas Detection System
+</a>
                     </li>
                     <li>
-                      <a href="#.">Download</a>
+                      <a href="swpms">Solar Pump Monitoring
+</a>
                     </li>
                     <li>
-                      <a href="#.">Sitemap</a>
+                      <a href="kms">Key Management
+System
+</a>
+                    </li>
+                       <li>
+                      <a href="fms">Facility Management</a>
                     </li>
                   </ul>
                 </div>
-                <div className="col-md-4 col-lg-4 mil-mb-60">
-                  <h4 className="mil-list-title mil-light mil-mb-60">Links</h4>
-                  <ul className="mil-hover-link-list mil-light">
-                    <li>
-                      <a href="#.">About Us</a>
-                    </li>
-                    <li>
-                      <a href="#.">Terms &amp; condition</a>
-                    </li>
-                    <li>
-                      <a href="#.">Privacy Policy</a>
-                    </li>
-                    <li>
-                      <a href="#.">Partners</a>
-                    </li>
-                    <li>
-                      <a href="#.">Press</a>
-                    </li>
-                  </ul>
-                </div>
+               
               </div>
             </div>
           </div>
@@ -122,7 +93,7 @@ const Footer2 = () => {
       </div>
       <div className="mil-footer-bottom">
         <div className="container">
-          <p className="mil-text-sm mil-light">© ITSulu 2023.</p>
+          <p className="mil-text-sm mil-light">© Entesla 2025.</p>
           <p className="mil-text-sm mil-light">All Rights Reserved.</p>
         </div>
       </div>
